8104bd7b394812646206f49039d19190e14d2df8
Richard Chen <richard.c.chen@gmail.com>
2018-07-29 01:23:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@
 
 def key_for_min_value(name_hash)
   currentloweset = name_hash.values[0]
+  lowestkey = ""
   name_hash.each do |key, value|
   if value < currentloweset
     currentlowest = value
